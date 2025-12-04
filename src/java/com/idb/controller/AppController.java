3ca5f74ac561882eb1623fe719b1ee7b225dab9f
salomeisima-source <salomeisima@gmail.com>
2025-12-04 19:39:12 +0300

    Update AppController.java
    space

--- src/java/com/idb/controller/AppController.java
+++ src/java/com/idb/controller/AppController.java
@@ -25,6 +25,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+
+
+
+
+
 @Controller
 public class AppController {
 
